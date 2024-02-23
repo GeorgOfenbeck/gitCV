d871b4f1eff658b40ca77734c01cb99f4b25042f
Georg Ofenbeck - taaofge1 <georg.ofenbeck@swisscom.com>
2024-02-23 13:55:52 +0000

    intermediate commit

--- src/scala/com/ofenbeck/gitcv/CV2Git.scala
+++ src/scala/com/ofenbeck/gitcv/CV2Git.scala
@@ -1,28 +1,74 @@
 package scala.com.ofenbeck.gitcv
 
-   import java.io.File
-   import org.eclipse.jgit.api.Git
-   import org.eclipse.jgit.lib.Repository
-   import org.eclipse.jgit.storage.file.FileRepositoryBuilder
-class CV2Git {
-  
-}
+import java.io.File
+import org.eclipse.jgit.api.Git
+import org.eclipse.jgit.lib.Repository
+import org.eclipse.jgit.storage.file.FileRepositoryBuilder
+import com.ofenbeck.gitcv.CV
+import com.ofenbeck.gitcv.main.cv
 
+    import scala.language.unsafeNulls
+    import zio.json.EncoderOps
+
+class CV2Git {}
 
 object CV2Git {
 
+// i want to create an empty git repository on a given path and create a file with the content of the CV on branch cv
+  def createGitRepositoryWithCV(path: String, cv: CV): Unit = {
+    val cvFile = new File(path + "/cv.json")
+    cvFile.createNewFile()
 
- // i want to create an empty git repository on a given path
- def createEmptyGitRepository(path: String): Unit = {
-   import scala.language.unsafeNulls
-   val repositoryPath = new File(path)
-   val builder = new FileRepositoryBuilder()
-   val repository = builder.setGitDir(repositoryPath)
-     .readEnvironment()
-     .findGitDir()
-     .build()
-   repository.create()
-   repository.close()
- }   
+    import java.nio.file.{Files, Paths}
+    val git = Git.init().setDirectory(new File(path)).call()
+    git.commit().setMessage("Initial commit").call()
+    git.branchCreate().setName("cv").call()
+    Files.write(Paths.get(cvFile.getAbsolutePath), cv.toJson.getBytes)
+    git.add().addFilepattern(path + "cv.json").call()
+    git.commit().setMessage("Initial commit").call()
+
+    // git.push().call()
+    println(path)
+  }
+
+  def branchEducation(cv: CV, git: Git): Unit = {
+    import scala.language.unsafeNulls
+    val branch = "Education"
+    git.checkout().setName(branch).call()
+    val education = cv.education
+    for (e <- education) {
+      val educationFile = new File(
+        git.getRepository.getDirectory.getParent + "/education/" + e.school + ".json"
+      )
+      educationFile.createNewFile()
+
+      import java.nio.file.{Files, Paths}
+      Files.write(Paths.get(educationFile.getAbsolutePath), e.toJson.getBytes)
+      git
+        .add()
+        .addFilepattern(
+          git.getRepository.getDirectory.getParent + "/education/" + e.school + ".json"
+        )
+        .call()
+      val commitmsg: String = """${e.title}
+      
+      ${e.description}
+      """ 
+      git.commit().setMessage(commitmsg).call()
+    }
+  }
+
+  // i want to create an empty git repository on a given path
+  def createEmptyGitRepository(path: String): Unit = {
+    import scala.language.unsafeNulls
+    val repositoryPath = new File(path)
+    val builder = new FileRepositoryBuilder()
+    val repository = builder
+      .setGitDir(repositoryPath)
+      .readEnvironment()
+      .findGitDir()
+      .build()
+    repository.create()
+    repository.close()
+  }
 }
-
--- src/scala/com/ofenbeck/gitcv/main.scala
+++ src/scala/com/ofenbeck/gitcv/main.scala
@@ -5,138 +5,173 @@ import com.ofenbeck.gitcv.CV
 import com.ofenbeck.gitcv.WorkExperince
 import java.time.LocalDate
 import com.ofenbeck.gitcv.Project
-
+import scala.com.ofenbeck.gitcv.CV2Git
 
 object main extends App {
 
   def SwisscomHealth = {
 
     val medicalPracticeManagmentSoftware = Project(
-        title = "Medical Practice Managment Software",
-        description = "I was responsible for improving scalability and maintainability issues. The software is used by ~1000 of medical practices in Switzerland.",
-        technologies = List(
-            Technology("Kubernetes, AKS, Cilium", "Improving scalability and maintainability issues"),
-            Technology("Azure managed services (Managed DB, Managed Storage Solutions)", "Migration from vm based solutions to managed services"),
+      title = "Medical Practice Managment Software",
+      description =
+        "I was responsible for improving scalability and maintainability issues. The software is used by ~1000 of medical practices in Switzerland.",
+      technologies = List(
+        Technology(
+          "Kubernetes, AKS, Cilium",
+          "Improving scalability and maintainability issues"
+        ),
+        Technology(
+          "Azure managed services (Managed DB, Managed Storage Solutions)",
+          "Migration from vm based solutions to managed services"
         )
+      )
     )
 
     val ragnarok = Project(
-        title = "Ragnarok",
-        description = "Design and implementation of a cloud native serverless application for a medical data messing system.",
-        technologies = List(
-            Technology("Kotlin Spring Cloud, Azure Functions", "A serverless redesign of the existing classic java application"),
-            Technology("Azure managed storage Solutions", "Using managed blob, table and queue storage for the serverless application"),
-            Technology("Open Telemetry", "Implementation of distributed tracing"),
-        )
+      title = "Ragnarok",
+      description =
+        "Design and implementation of a cloud native serverless application for a medical data messing system.",
+      technologies = List(
+        Technology(
+          "Kotlin Spring Cloud, Azure Functions",
+          "A serverless redesign of the existing classic java application"
+        ),
+        Technology(
+          "Azure managed storage Solutions",
+          "Using managed blob, table and queue storage for the serverless application"
+        ),
+        Technology("Open Telemetry", "Implementation of distributed tracing")
+      )
     )
     val devOps = Project(
-        title = "DevOps",
-        description = "Modernizing or creating new CI/CD pipelines for various products",
-        technologies = List(
-            Technology("Azure DevOps", "Migration from Jenkins to Azure DevOps"),
-            Technology("Terraform", "Migration from ARM templates and ansible to Terraform"),
-            Technology("Dev Container / Dev Setup", "Lowering the barrier for new developers to start working on the project and creating faster feedback loops"), 
+      title = "DevOps",
+      description =
+        "Modernizing or creating new CI/CD pipelines for various products",
+      technologies = List(
+        Technology("Azure DevOps", "Migration from Jenkins to Azure DevOps"),
+        Technology(
+          "Terraform",
+          "Migration from ARM templates and ansible to Terraform"
+        ),
+        Technology(
+          "Dev Container / Dev Setup",
+          "Lowering the barrier for new developers to start working on the project and creating faster feedback loops"
         )
+      )
     )
 
     WorkExperince(
-        end = LocalDate.of(2024, 4, 1).nn,
-        start = LocalDate.of(2022, 2, 1).nn,
-        title = "Senior Cloud Architect",
-        company = "Swisscom Health",
-        description = "Implementation of a big data pipeline for machine learning. Implementation of a high throughput streaming application.",
-        projects = List(
-            medicalPracticeManagmentSoftware,
-            devOps,
-            ragnarok,
-        ),
+      end = LocalDate.of(2024, 4, 1).nn,
+      start = LocalDate.of(2022, 2, 1).nn,
+      title = "Senior Cloud Architect",
+      company = "Swisscom Health",
+      description =
+        "Implementation of a big data pipeline for machine learning. Implementation of a high throughput streaming application.",
+      projects = List(
+        medicalPracticeManagmentSoftware,
+        devOps,
+        ragnarok
+      )
     )
   }
 
   def SwisscomDNA = {
     WorkExperince(
-        end = LocalDate.of(2022, 1, 31).nn,
-        start = LocalDate.of(2018, 4, 3).nn,
-        title = "Senior Software Engineer",
-        company = "Data, Analytics und AI Department, Swisscom",
-        description = "Implementation of a big data pipeline for machine learning. Implementation of a high throughput streaming application.",
-        projects = List(
-            Project(
-                title = "Big Data Pipeline for Machine Learning",
-                description = "Implementing features and improving performance on the ETL data pipeline for ML applications. Reduced the Dev Cycle from larger than a working day to an hour.",
-                technologies = List(
-                    Technology("Apache Spark, HDFS, Cloud Foundary", "working on two different on-premises big data applications"),
-                )
-            ),
-            Project(
-                title = "High Throughput Streaming Application",
-                description = "Implementation and operation of a real-time streaming application processing 6k msgs/s triggering interactions with customers.",
-                technologies = List(
-                    Technology("Kafka, Akka Streaming, Flink", ""),
-                )
+      end = LocalDate.of(2022, 1, 31).nn,
+      start = LocalDate.of(2018, 4, 3).nn,
+      title = "Senior Software Engineer",
+      company = "Data, Analytics und AI Department, Swisscom",
+      description =
+        "Implementation of a big data pipeline for machine learning. Implementation of a high throughput streaming application.",
+      projects = List(
+        Project(
+          title = "Big Data Pipeline for Machine Learning",
+          description =
+            "Implementing features and improving performance on the ETL data pipeline for ML applications. Reduced the Dev Cycle from larger than a working day to an hour.",
+          technologies = List(
+            Technology(
+              "Apache Spark, HDFS, Cloud Foundary",
+              "working on two different on-premises big data applications"
             )
+          )
+        ),
+        Project(
+          title = "High Throughput Streaming Application",
+          description =
+            "Implementation and operation of a real-time streaming application processing 6k msgs/s triggering interactions with customers.",
+          technologies = List(
+            Technology("Kafka, Akka Streaming, Flink", "")
+          )
         )
+      )
     )
   }
 
   val ethPhd = Education(
-      end = LocalDate.of(2017, 5, 1).nn,
-      start = LocalDate.of(2011, 10, 1).nn,
-      title = "PhD, Computer Science",
-      school = "ETH Zurich",
-      description = "I worked on the topic of appling generic programming techniques to challenges in metaprogramming in the context of high performance code generators.", 
+    end = LocalDate.of(2017, 5, 1).nn,
+    start = LocalDate.of(2011, 10, 1).nn,
+    title = "PhD, Computer Science",
+    school = "ETH Zurich",
+    description =
+      "I worked on the topic of appling generic programming techniques to challenges in metaprogramming in the context of high performance code generators."
   )
   val ethPhd0 = Education(
-      end = LocalDate.of(2011, 10, 1).nn,
-      start = LocalDate.of(2010, 8, 1).nn,
-      title = "PhD student, Computer Science",
-      school = "ETH Zurich",
-      description = "Before switching topic, I worked my first year on enabling micro transaction support in the kernel of an OS",
+    end = LocalDate.of(2011, 10, 1).nn,
+    start = LocalDate.of(2010, 8, 1).nn,
+    title = "PhD student, Computer Science",
+    school = "ETH Zurich",
+    description =
+      "Before switching topic, I worked my first year on enabling micro transaction support in the kernel of an OS"
   )
   val ethMsc = Education(
-      end = LocalDate.of(2009, 11, 1).nn,
-      start = LocalDate.of(2007, 9, 1).nn,
-      title = "MSc, Computational Biology and Bioinformatics",
-      school = "ETH Zurich",
-      description = "",
+    end = LocalDate.of(2009, 11, 1).nn,
+    start = LocalDate.of(2007, 9, 1).nn,
+    title = "MSc, Computational Biology and Bioinformatics",
+    school = "ETH Zurich",
+    description = ""
   )
 
   val tuBsc = Education(
-      end = LocalDate.of(2007, 6, 1).nn,
-      start = LocalDate.of(2003, 9, 1).nn,
-      title = "BSc, Medical Informatics",
-      school = "Technical University Vienna",
-      description = "",
+    end = LocalDate.of(2007, 6, 1).nn,
+    start = LocalDate.of(2003, 9, 1).nn,
+    title = "BSc, Medical Informatics",
+    school = "Technical University Vienna",
+    description = ""
   )
 
   val vieBsc = Education(
-      end = LocalDate.of(2007, 6, 1).nn,
-      start = LocalDate.of(2003, 9, 1).nn,
-      title = "Diploma Student, Molecular Biology",
-      school = "Univerity of Vienna",
-      description = "",
+    end = LocalDate.of(2007, 6, 1).nn,
+    start = LocalDate.of(2003, 9, 1).nn,
+    title = "Diploma Student, Molecular Biology",
+    school = "Univerity of Vienna",
+    description = ""
   )
 
   val htl = Education(
-      end = LocalDate.of(2003, 7, 1).nn,
-      start = LocalDate.of(1998, 9, 1).nn,
-      title = "HTL, IT and Organization",
-      school = "HTL Pinkafeld",
-      description = "",
+    end = LocalDate.of(2003, 7, 1).nn,
+    start = LocalDate.of(1998, 9, 1).nn,
+    title = "HTL, IT and Organization",
+    school = "HTL Pinkafeld",
+    description = ""
   )
 
+  val cv = CV(
+    "Georg Ofenbeck",
+    "",
+    "",
+    List(
+      SwisscomHealth,
+      SwisscomDNA
+    ),
+    List(ethPhd, ethPhd0, ethMsc, tuBsc, vieBsc, htl)
+  )
 
-  val cv = CV("Georg Ofenbeck", "", "", 
-  List(
-    SwisscomHealth,
-    SwisscomDNA
-  ), List(
-    ethPhd,
-    ethPhd0,
-    ethMsc,
-    tuBsc,
-    vieBsc,
-    htl))
-  
   println(s"Hello World, ${cv.toJson}")
+
+  // path is a temporary directory in the temporary folder of the system
+  val path = java.nio.file.Files.createTempDirectory("temp").toString
+
+  CV2Git.createGitRepositoryWithCV(path, cv)
+
+//   CV2Git.createGitRepositoryWithCV("/tmp/gitcv", cv)
 }
